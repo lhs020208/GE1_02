7ef636126931715c5b161d0d63007cea7e65da16
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2025-05-25 22:03:05 +0900

    Racing 제작중

---
+++ Assets/script/Racing/Player/Player_Reset_R.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class Player_Reset_R : MonoBehaviour
+{
+    private Vector3 initPosition;
+    private Quaternion initRotation;
+    private Rigidbody rb;
+
+    public GameObject Ceiling;
+    private Vector3 ceilingInitPosition;
+    private Quaternion ceilingInitRotation;
+
+    void Start()
+    {
+        initPosition = transform.position;
+        initRotation = transform.rotation;
+
+        if (Ceiling != null)
+        {
+            ceilingInitPosition = Ceiling.transform.position;
+            ceilingInitRotation = Ceiling.transform.rotation;
+        }
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (transform.position.y <= -10.0f)
+        {
+            ResetTransform();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "SavePoint")
+        {
+            initPosition = transform.position;
+            initRotation = transform.rotation;
+
+            if (Ceiling != null)
+            {
+                ceilingInitPosition = Ceiling.transform.position;
+                ceilingInitRotation = Ceiling.transform.rotation;
+            }
+            Destroy(other.gameObject);
+        }
+    }
+
+    void ResetTransform()
+    {
+        transform.position = initPosition;
+        transform.rotation = initRotation;
+
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
+        if (Ceiling != null)
+        {
+            Ceiling.transform.position = ceilingInitPosition;
+            Ceiling.transform.rotation = ceilingInitRotation;
+        }
+    }
+}
